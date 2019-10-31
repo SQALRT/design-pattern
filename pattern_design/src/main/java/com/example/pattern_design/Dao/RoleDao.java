ddb2b721ba1af7e981987bc01d8f0f3ba7ed64bd
2947764906@qq.com <41712691+SQALRT@users.noreply.github.com>
2019-10-31 21:25:55 +0800

    third commit

---
+++ pattern_design/src/main/java/com/example/pattern_design/Dao/RoleDao.java
@@ -0,0 +1,8 @@
+package com.example.pattern_design.Dao;
+import com.example.pattern_design.entity.Role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface RoleDao extends JpaRepository<Role, Long>{
+    public Role findBydescription(String description);
+    // public Trans findByDriver(Driver driver);
+}
